47d2635e9cf9212bf41a0aadc1490f716f69c691
rinteri <137144699+rinteri@users.noreply.github.com>
2023-06-22 14:01:03 +0700

    Add files via upload
    Что бы понять, является ли слово палиндромом, я сравниваю первый символ и последний, после этого второй с предпоследним и так далее.

--- hw1.py
+++ hw1.py
@@ -1,14 +1,6 @@
-def strcounter(string): #сложность O(n * m)
-    for symvol in set(string): 
-        counter = 0
-        for subsymvol in string:
-            if symvol == subsymvol:
-                counter += 1
-    if counter == 2:
-        print(True)
-    else:
-        print(False)
-            
-strcounter('abcde')
-strcounter('abffba')
+def palindrome():
+    s = s[::-1]
 
+while True:
+    s = input("Введите слово: ")
+    print(True if palindrome(s) else False)
